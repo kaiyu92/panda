4f7212ab0b17e303e6db2a06c73959e480427299
Can Acar <canacar@imcan.dev>
2024-11-26 22:32:31 +0100

    fix inline redefine for clang.
    old version broke some usages of the inline keyword (e.g. inline namespace)

--- include/qemu/int128.h
+++ include/qemu/int128.h
@@ -10,145 +10,148 @@ typedef __int128_t Int128;
 // Without inlining, clang creates function calls that return structures
 // { i64, i64 }.  This complicates taint propagation.
 // TODO: make clang -O1 aggressively inline like clang -O2 does, then remove
-// the next four lines.
+// the next four lines. The usages of `PANDA_ALWAYS_INLINE_CLANG` are just `inline`
+// in the definitions below on upstream.
 #ifdef __clang__
-#define inline __attribute__((always_inline))
+#define PANDA_ALWAYS_INLINE_CLANG inline __attribute__((always_inline))
 #pragma clang diagnostic ignored "-Wunused-function"
+#else
+#define PANDA_ALWAYS_INLINE_CLANG inline
 #endif
 
-static inline Int128 int128_make64(uint64_t a)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_make64(uint64_t a)
 {
     return a;
 }
 
-static inline Int128 int128_make128(uint64_t lo, uint64_t hi)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_make128(uint64_t lo, uint64_t hi)
 {
     return (__uint128_t)hi << 64 | lo;
 }
 
-static inline uint64_t int128_get64(Int128 a)
+static PANDA_ALWAYS_INLINE_CLANG uint64_t int128_get64(Int128 a)
 {
     uint64_t r = a;
     assert(r == a);
     return r;
 }
 
-static inline uint64_t int128_getlo(Int128 a)
+static PANDA_ALWAYS_INLINE_CLANG uint64_t int128_getlo(Int128 a)
 {
     return a;
 }
 
-static inline int64_t int128_gethi(Int128 a)
+static PANDA_ALWAYS_INLINE_CLANG int64_t int128_gethi(Int128 a)
 {
     return a >> 64;
 }
 
-static inline Int128 int128_zero(void)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_zero(void)
 {
     return 0;
 }
 
-static inline Int128 int128_one(void)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_one(void)
 {
     return 1;
 }
 
-static inline Int128 int128_2_64(void)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_2_64(void)
 {
     return (Int128)1 << 64;
 }
 
-static inline Int128 int128_exts64(int64_t a)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_exts64(int64_t a)
 {
     return a;
 }
 
-static inline Int128 int128_and(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_and(Int128 a, Int128 b)
 {
     return a & b;
 }
 
-static inline Int128 int128_rshift(Int128 a, int n)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_rshift(Int128 a, int n)
 {
     return a >> n;
 }
 
-static inline Int128 int128_add(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_add(Int128 a, Int128 b)
 {
     return a + b;
 }
 
-static inline Int128 int128_neg(Int128 a)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_neg(Int128 a)
 {
     return -a;
 }
 
-static inline Int128 int128_sub(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_sub(Int128 a, Int128 b)
 {
     return a - b;
 }
 
-static inline bool int128_nonneg(Int128 a)
+static PANDA_ALWAYS_INLINE_CLANG bool int128_nonneg(Int128 a)
 {
     return a >= 0;
 }
 
-static inline bool int128_eq(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG bool int128_eq(Int128 a, Int128 b)
 {
     return a == b;
 }
 
-static inline bool int128_ne(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG bool int128_ne(Int128 a, Int128 b)
 {
     return a != b;
 }
 
-static inline bool int128_ge(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG bool int128_ge(Int128 a, Int128 b)
 {
     return a >= b;
 }
 
-static inline bool int128_lt(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG bool int128_lt(Int128 a, Int128 b)
 {
     return a < b;
 }
 
-static inline bool int128_le(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG bool int128_le(Int128 a, Int128 b)
 {
     return a <= b;
 }
 
-static inline bool int128_gt(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG bool int128_gt(Int128 a, Int128 b)
 {
     return a > b;
 }
 
-static inline bool int128_nz(Int128 a)
+static PANDA_ALWAYS_INLINE_CLANG bool int128_nz(Int128 a)
 {
     return a != 0;
 }
 
-static inline Int128 int128_min(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_min(Int128 a, Int128 b)
 {
     return a < b ? a : b;
 }
 
-static inline Int128 int128_max(Int128 a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG Int128 int128_max(Int128 a, Int128 b)
 {
     return a > b ? a : b;
 }
 
-static inline void int128_addto(Int128 *a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG void int128_addto(Int128 *a, Int128 b)
 {
     *a += b;
 }
 
-static inline void int128_subfrom(Int128 *a, Int128 b)
+static PANDA_ALWAYS_INLINE_CLANG void int128_subfrom(Int128 *a, Int128 b)
 {
     *a -= b;
 }
 
-static inline Int128 bswap128(Int128 a)
+static PANDA_ALWAYS_INLINE_CLANG Int128 bswap128(Int128 a)
 {
     return int128_make128(bswap64(int128_gethi(a)), bswap64(int128_getlo(a)));
 }
